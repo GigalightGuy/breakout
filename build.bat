883e27f7d69bb08660b414e58c0ab73381e5ba43
GigalightGuy <80604755+GigalightGuy@users.noreply.github.com>
2025-01-26 19:30:40 +0000

    Rename main.cpp to game.cpp

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 
 pushd %~dp0
-clang++ -o breakout.exe code/main.cpp -g -Wall -Wextra -Werror -Wno-unused-function -luser32.lib -lgdi32.lib
+clang++ -o breakout.exe code/game.cpp -g -Wall -Wextra -Werror -Wno-unused-function -luser32.lib -lgdi32.lib
 popd
